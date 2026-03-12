b2f36d563766cb882a42e23ec8bc6d8a88437357
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2026-03-12 14:52:05 +0900

    chore: Initialize Kotlin Spring Boot account-service project

---
+++ account-service/src/main/kotlin/com/example/account_service/AccountServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.account_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccountServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<AccountServiceApplication>(*args)
+}
---
+++ account-service/src/main/kotlin/com/example/account_service/controller/HelloController.kt
@@ -0,0 +1,13 @@
+package com.example.account_service.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "bank api running"
+    }
+}
