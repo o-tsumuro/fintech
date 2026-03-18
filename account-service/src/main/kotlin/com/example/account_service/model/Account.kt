e90efc2d5f7de1e40da382436010a28aada2faa4
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2026-03-18 11:41:55 +0900

    feat: add Account entity

---
+++ account-service/src/main/kotlin/com/example/account_service/model/Account.kt
@@ -0,0 +1,16 @@
+package com.example.account_service.model
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "accounts")
+data class Account(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    val name: String,
+
+    val balance: Long
+)
