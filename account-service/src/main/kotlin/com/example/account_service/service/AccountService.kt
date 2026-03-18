d17240aa50310a3d0a4e49ec598aa597fee48e8f
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2026-03-18 14:20:02 +0900

    feat: implement deposit logic in AccountService

--- account-service/src/main/kotlin/com/example/account_service/service/AccountService.kt
+++ account-service/src/main/kotlin/com/example/account_service/service/AccountService.kt
@@ -2,6 +2,7 @@ package com.example.account_service.service
 
 import com.example.account_service.repository.AccountRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AccountService(
@@ -12,5 +13,19 @@ class AccountService(
             .orElseThrow { RuntimeException("account no found") }
 
         return account.balance
+    }
+
+    @Transactional
+    fun deposit(accountId: Long, amount: Long) {
+        if (amount <= 0) {
+            throw RuntimeException("invalid amount")
+        }
+
+        val account = accountRepository.findById(accountId)
+            .orElseThrow { RuntimeException("account not found") }
+
+        account.balance += amount
+
+        accountRepository.save(account)
     }
 }
