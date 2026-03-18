885eaa5c2f3f2e38dda0214a2b7226cfe4cb0c3a
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2026-03-18 13:27:11 +0900

    chore: remove H2 dependency from build configuration

--- account-service/build.gradle.kts
+++ account-service/build.gradle.kts
@@ -26,7 +26,6 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("tools.jackson.module:jackson-module-kotlin")
 	runtimeOnly("org.postgresql:postgresql")
-	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
