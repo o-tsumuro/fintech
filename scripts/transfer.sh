e44fcdb1dc6b7e7386a996a627f7d0c5a9e78b5a
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2026-04-23 12:37:54 +0900

    chore: update transfer script to include idempotency key

--- scripts/transfer.sh
+++ scripts/transfer.sh
@@ -4,10 +4,13 @@ FROM_ACCOUNT_ID=1
 TO_ACCOUNT_ID=2
 AMOUNT=1000
 
+IDEMPOTENCY_KEY=$(uuidgen)
+
 curl -X POST http://localhost:8080/accounts/transfer \
 -H "Content-Type: application/json" \
 -d "{
   \"fromAccountId\": $FROM_ACCOUNT_ID,
   \"toAccountId\": $TO_ACCOUNT_ID,
-  \"amount\": $AMOUNT
+  \"amount\": $AMOUNT,
+  \"idempotencyKey\": \"$IDEMPOTENCY_KEY\"
 }"
